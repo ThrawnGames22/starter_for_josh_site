db7dabdc8c4ed52d940a9497604a07f905363db9
ThrawnGames <113761369+ThrawnGames22@users.noreply.github.com>
2024-03-24 11:03:32 +0800

    navwalker

--- page_header.php
+++ page_header.php
@@ -44,37 +44,21 @@
         <!-- Brand -->
         <a class="navbar-brand" href="#"><span class = "josh">JOSH</span> <span class ="orange">WHITKIN</span> </a>
       
-        <!-- Toggler/collapsibe Button -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+                <!-- Toggler/collapsibe Button -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+    </button>
       
-        <!-- Navbar links -->
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">HOME</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">PROJECTS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">PUBLICATIONS</a>
-            </li>
+        <?php 
 
-            <li class="nav-item">
-                <a class="nav-link" href="#">CV</a>
-            </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#">CONTACT</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div class = "smallpage-tag">
-      <h1 class ="smallpage-title">Projects</h1>
-    </div>
+
+?>
+       
+    </nav>
+    <div class = "smallpage-tag">
+       <h1 class = "smallpage-title">Projects</h1>
+    
     </div> <!-- container -->
     <?php wp_head(); ?> 
 </header>
